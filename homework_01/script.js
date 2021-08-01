5b01c9999b1be33484691212c983a3a265c9c1dd
AlexanderBil <63557206+AlexanderBil@users.noreply.github.com>
2021-08-01 18:40:24 +0300

    hw.01

---
+++ homework_01/script.js
@@ -0,0 +1,107 @@
+//task2
+// console.log("Bilopol");
+
+//-----------------------
+
+//task3
+// //a
+// let a;
+// let b;
+
+// //b
+// a = 'Tor';
+// b = 'Odin';
+
+// //c
+// alert(`Variable a: ${a}`);
+// alert(`Variable b: ${b}`);
+
+// //d
+// b = a;
+
+// //e
+// alert(`Variable a: ${a}`);
+// alert(`Variable b: ${b}`);
+
+//----------------------
+
+//task4
+// const user = {
+//     "String": 'Alex',
+//     "Number": 20,
+//     "Boolean": true,
+//     "Undefined": undefined,
+//     "Null": null,
+// }
+
+//----------------------
+
+//task5
+// const isAdult = confirm("Are you 18 years old?");
+// console.log(isAdult);
+
+//----------------------
+
+//task6
+// let first_name;
+// let second_name;
+// let group;
+// let birth_year;
+
+// first_name = "Oleksandr";
+// second_name = "Bilopol";
+// group = "620_JS";
+// birth_year = 1988;
+
+// const married = true;
+
+// console.log(birth_year);
+// console.log(married);
+// console.log(first_name);
+// console.log(second_name);
+// console.log(group);
+
+// let n = null;
+// let u = undefined;
+
+// console.log(typeof n);
+// console.log(typeof u);
+
+//-----------------------
+
+//task7
+// let login = prompt("Enter login");
+// let email = prompt("Enter email");
+// let password = prompt("Enter passwor");
+
+// alert(`Dear User, your login is ${login}, your email is ${email}, your password is ${password}`)
+
+//--------------------
+
+//task8
+// const button = document.querySelector('.button');
+
+// button.onclick = function(){
+//     let select = document.querySelector('.select').value;
+//     let hour;
+//     let day;
+//     let month;
+
+//     switch(select){
+//         case '1':
+//         hour = 60*60
+//         alert(`In an hour: ${hour} seconds`)
+//         break
+
+//         case '2':
+//         day = 60*60*24
+//         alert(`In the day: ${day} seconds`)
+//         break
+
+//         case '3':
+//         month = 60*60*24*30 
+//         alert(`In a month: ${month} seconds`)
+//         break
+//     }
+// }
+
